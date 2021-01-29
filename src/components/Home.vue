48491cd4f070849340bb7fc3e2061ead05384fe2
donaldhaxhiaj <donald_haxhiaj@yahoo.com>
2021-01-29 16:37:34 +0100

    fixes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,28 +3,39 @@
     <b-col cols="8">
       <div>
         <b-nav tabs fill justified>
-          <b-nav-item style="background: #0096c7;" @click="addTable()"><p class="text-white">Add</p></b-nav-item>
-          <b-nav-item style="background: #ef476f;" v-if="!isEdit" @click="editTables()"><p class="text-white">Edit</p></b-nav-item>
-          <b-nav-item style="background: #16bf82;" v-else @click="doneEdit()"><p class="text-white">Done edit</p></b-nav-item>
+          <b-nav-item style="background: #0096c7;" @click="addTable()" :disabled="isEdit"><p class="text-white">Shto tavoline</p>
+          </b-nav-item>
+          <b-nav-item style="background: #ef476f;" v-if="!isEdit" @click="editTables()"><p class="text-white">Modifiko tavoline</p>
+          </b-nav-item>
+          <b-nav-item style="background: #16bf82;" v-else @click="doneEdit()"><p class="text-white">Perfundo modifikimet</p>
+          </b-nav-item>
         </b-nav>
       </div>
       <b-card style="height: calc(100vh - 158px);">
-        <b-card-title>Tavolinat</b-card-title>
-        <div id="main" style="padding-left: 149px;padding-top: 24px;" v-for="item in tables">
-          <VueDragResize :class="isEdit ? 'shake' : ''" :sticks="[]" :id="'tooltip-target-' + item.key"
-                         style="box-shadow: 0 1px 6px 0 rgba(32, 33, 36, .28); background: #506dfb;border-radius: 20px; cursor: pointer"
-                         :isActive="isEdit" :preventActiveBehavior="true" :w="80"
-                         :h="80" :y="item.top" :x="item.left" :z="0" v-on:dragging="resize($event,item.key,item.tableName)">
+        <div id="main" class="tableMain" v-for="item in tables">
+          <VueDragResize :class="isEdit ? 'shake' : ''"
+                         :sticks="[]"
+                         :id="'tooltip-target-' + item.key"
+                         class="tableStyles"
+                         :isActive="isEdit"
+                         :preventActiveBehavior="true" :w="80"
+                         :parentLimitation="true"
+                         :h="80" :y="item.top" :x="item.left" :z="0"
+                         :parentH="1000"
+                         :parentW="1200"
+                         v-on:dragging="resize($event,item.key,item.tableName)">
 
-              <b-icon icon="x-circle" v-if="isEdit" variant="danger" @click="removeTable(item.key)" title="Fshi tavolinen"></b-icon>
+            <b-icon icon="x-circle" v-if="isEdit" variant="danger" @click="removeTable(item.key)"
+                    title="Fshi tavolinen"></b-icon>
 
-            <div class="text-center" >
+            <div class="text-center" style="margin-top: 21px;">
               <h5 class="text-white">{{ item.tableName }}</h5>
             </div>
           </VueDragResize>
           <div v-if="isEdit">
             <b-popover :target="'tooltip-target-' + item.key" triggers="click" placement="top">
-              <input class="form-control" style="background-color: transparent;border-radius: 25px;" type="text" v-model="item.tableName" placeholder="Emri i tavolines" @blur="tableNameChange(item.key,item.tableName)"/>
+              <input class="form-control inputTableName" type="text" v-model="item.tableName"
+                     placeholder="Emri i tavolines" @blur="tableNameChange(item.key,item.tableName)"/>
             </b-popover>
           </div>
         </div>
@@ -104,12 +115,10 @@ export default {
       table.left = newRect.left;
     },
 
-    tableNameChange(key,tableName){
-      console.log('test')
+    tableNameChange(key, tableName) {
       let tables = JSON.parse(localStorage.getItem('tables')).filter(x => x.tableName === tableName)
-
-      if(tables.length > 0)
-        alert('Tavolina me emrin '+ tableName +' ekziston!')
+      if (tables.length > 0)
+        alert('Tavolina me emrin ' + tableName + ' ekziston!')
 
       let table = this.tables.find(x => x.key === key);
 
@@ -120,12 +129,13 @@ export default {
       let table = {
         key: Math.floor(Math.random() * 100) + 1,
         profileId: this.selectedProfile,
-        top: 100,
-        left: 100
+        top: Math.floor(Math.random() * 100) + 1,
+        left: Math.floor(Math.random() * 100) + 1
       }
       this.tables = (localStorageItem !== null && localStorageItem.length > 0) ? localStorageItem : []
       this.tables.push(table)
       localStorage.setItem('tables', JSON.stringify(this.tables))
+      this.selectProfile(this.selectedProfile)
     },
     editTables() {
       this.isEdit = true
@@ -168,7 +178,7 @@ export default {
     addProfile() {
       if (this.profileName === '')
         return false
-      this.profiles.push({Id: Math.floor(Math.random() * 20) + 1, name: this.profileName}),
+      this.profiles.push({Id: Math.floor(Math.random() * 20) + 1, name: this.profileName})
       this.selectProfile(this.selectedProfile)
     },
     selectProfile(profileId) {
@@ -199,6 +209,23 @@ export default {
 
 <style scoped>
 
+.tableMain {
+  padding-left: 149px;
+  padding-top: 24px;
+}
+
+.tableStyles {
+  box-shadow: 0 1px 6px 0 rgba(32, 33, 36, .28);
+  background: #506dfb;
+  border-radius: 20px;
+  cursor: pointer
+}
+
+.inputTableName {
+  background-color: transparent;
+  border-radius: 25px;
+}
+
 .b-icon {
   float: right;
   cursor: pointer;
@@ -208,23 +235,29 @@ export default {
 .vdr.active:before {
   outline: none !important;
 }
-.tooltip>.tooltip-inner {
+
+.tooltip > .tooltip-inner {
   background-color: transparent !important;
 }
+
 .shake {
-  animation-delay: -.75s; animation-duration: .25s;
+  animation-delay: -.75s;
+  animation-duration: .25s;
 }
+
 #main .vdr:nth-child(2n) {
   animation-name: keyframes1;
   animation-iteration-count: infinite;
   transform-origin: 50% 10%;
 }
+
 #main .vdr:nth-child(2n-1) {
   animation-name: keyframes2;
   animation-iteration-count: infinite;
   animation-direction: alternate;
   transform-origin: 30% 5%;
 }
+
 @keyframes keyframes1 {
   0% {
     transform: rotate(-1deg);
@@ -236,6 +269,7 @@ export default {
     animation-timing-function: ease-out;
   }
 }
+
 @keyframes keyframes2 {
   0% {
     transform: rotate(1deg);
