604c119b3a5b2dc67daa673e97806c5b552b0f4c
Reidi Qyrku <reidiqyrku@hotmail.com>
2021-01-30 00:34:38 +0100

    improvements

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,7 +6,7 @@
           <div
             id="main"
             class="tableMain"
-            v-for="(item, index) in tables"
+            v-for="(item, index) in profileTables"
             :key="index"
           >
             <VueDragResize
@@ -24,7 +24,7 @@
               :z="0"
               :parentH="1000"
               :parentW="1600"
-              @dragstop="changePosition($event, item.key, item.tableName)"
+              @dragstop="changePosition($event, item.key)"
             >
               <b-icon
                 icon="x-circle"
@@ -38,7 +38,7 @@
                 <h5 class="text-white">{{ item.tableName }}</h5>
               </div>
             </VueDragResize>
-            <div v-if="isEdit">
+            <!-- <div v-if="isEdit">
               <b-popover
                 :target="'tooltip-target-' + item.key"
                 triggers="click"
@@ -49,44 +49,40 @@
                   type="text"
                   v-model="item.tableName"
                   placeholder="Emri i tavolines"
-                  @blur="tableNameChange(item.key, item.tableName)"
                 />
               </b-popover>
-            </div>
+            </div> -->
           </div>
         </b-col>
         <b-col cols="2">
           <div class="form-group">
             <b-button
               block
+              pill
               variant="primary"
               @click="editTables()"
               v-if="!isEdit"
               >Modifiko Planimetrin</b-button
             >
-            <b-button block v-if="isEdit" variant="success" @click="doneEdit()"
+            <b-button block pill v-if="isEdit" variant="success" @click="doneEdit()"
               >Perfundo modifikimet</b-button
             >
-            <b-button block v-if="isEdit" @click="addTable()"
+            <b-button block pill v-if="isEdit" @click="addTable()"
               >Shto Tavoline</b-button
             >
-            <b-button block @click="showModal">Shto Planimetri</b-button>
+            <b-button pill block @click="showModal">Shto Planimetri</b-button>
           </div>
-          <div class="form-group">
-            <b-list-group>
-              <b-list-group-item
-                active-class="active"
-                v-for="(profile, index) in profiles"
-                @click="selectProfile(profile.Id)"
-                style="cursor: pointer"
-                :key="index"
-              >
-                <span>
-                  {{ profile.name }}
-                </span>
-              </b-list-group-item>
-            </b-list-group>
-          </div>
+          <b-card
+            v-for="(profile, index) in profiles"
+            :key="index"
+            class="mb-2 text-center"
+            @click="selectProfile(profile.id)"
+            style="cursor: pointer"
+          >
+            <b-card-text>
+              {{ profile.name }}
+            </b-card-text>
+          </b-card>
         </b-col>
       </b-row>
     </b-card>
@@ -94,16 +90,15 @@
       <div class="d-block text-center">
         <b-form @submit.stop.prevent>
           <b-form-input
-            :state="validation"
             id="feedback-user"
             v-model="profileName"
             placeholder="Emri i profilit"
           ></b-form-input>
-          <b-form-invalid-feedback :state="validation">
+          <b-form-invalid-feedback>
             Emri i profilit duhet te jete 5-12 karaktere i gjate dhe te jete i
             ndryshem nga profilet e tjera.
           </b-form-invalid-feedback>
-          <b-form-valid-feedback :state="validation">
+          <b-form-valid-feedback>
             Eshte ne rregull.
           </b-form-valid-feedback>
         </b-form>
@@ -114,7 +109,6 @@
       <b-button
         class="mt-2"
         variant="outline-warning"
-        :disabled="validation === false"
         block
         @click="addProfile()"
       >
@@ -128,34 +122,43 @@
 import VueDragResize from "vue-drag-resize";
 
 export default {
-  name: "Home",
+  name: "TableManager",
   components: {
     VueDragResize,
+  },
+  created() {
+    this.profiles = JSON.parse(localStorage.getItem("ordis:planes")) || [];
+    if (this.profiles.length) {
+      this.selectedProfile = this.profiles.find((profile) => profile.default);
+      if (!this.selectedProfile) {
+        this.selectedProfile = this.profiles.sort((a, b) => a.id - b.id)[0].id;
+      }
+      this.tables = JSON.parse(localStorage.getItem("ordis:tables")) || [];
+    }
   },
   data() {
     return {
       lastDeleted: 0,
       isEdit: false,
       profileName: "",
-      profiles: [
-        { Id: 1, name: "test" },
-        { Id: 2, name: "test2" },
-      ],
+      profiles: [],
       tables: [],
       selectedProfile: 0,
       filteredTables: [],
     };
   },
-
   methods: {
-    changePosition(newRect, item) {
-      let table = this.tables.find((x) => x.key === item);
-      table.top = newRect.top;
-      table.left = newRect.left;
+    changePosition(newPosition, id) {
+      let index = this.tables.indexOf(table => table.key === id)
+      let table = this.tables.find((x) => x.key === id);
+      table.top = newPosition.top;
+      table.left = newPosition.left;
+      this.tables.slice(index, 1, table);
+      localStorage.setItem("ordis:tables", JSON.stringify(this.tables));
     },
 
     tableNameChange(key, tableName) {
-      let tables = JSON.parse(localStorage.getItem("tables")).filter(
+      let tables = JSON.parse(localStorage.getItem("ordis:tables")).filter(
         (x) => x.tableName === tableName
       );
       if (tables.length > 0)
@@ -166,86 +169,39 @@ export default {
       table.tableName = tableName;
     },
     addTable() {
-      if (localStorage.getItem("tables") === null)
-        localStorage.setItem("tables", JSON.stringify([]));
-
-      let localStorageItem = JSON.parse(localStorage.getItem("tables")).filter(
-        (x) => x != null
-      );
       let table = {
         key: Math.floor(Math.random() * 100) + 1,
         profileId: this.selectedProfile,
         top: Math.floor(Math.random() * 100) + 1,
         left: Math.floor(Math.random() * 100) + 1,
       };
-      this.tables =
-        localStorageItem !== null && localStorageItem.length > 0
-          ? localStorageItem
-          : [];
       this.tables.push(table);
-      localStorage.setItem("tables", JSON.stringify(this.tables));
-      this.selectProfile(this.selectedProfile);
+      localStorage.setItem("ordis:tables", JSON.stringify(this.tables));
     },
     editTables() {
       this.isEdit = true;
     },
-    tablesJoining() {
-      let self = this;
-      let tableFromStorage = JSON.parse(localStorage.getItem("tables")).filter(
-        (x) => x !== null && this.lastDeleted !== x.key
-      );
-      if (tableFromStorage.length > 0) {
-        tableFromStorage.forEach(function (item) {
-          self.tables.push(item);
-        });
-      }
-      let unique = Array.from(new Set(this.tables.map((x) => x.key))).map(
-        (key) => {
-          let tableEntity = this.tables.find((x) => x.key === key);
-          return {
-            key: key,
-            tableName: tableEntity.tableName,
-            top: tableEntity.top,
-            left: tableEntity.left,
-            profileId: tableEntity.profileId,
-          };
-        }
-      );
-
-      localStorage.setItem("tables", JSON.stringify(unique));
-      this.tables = JSON.parse(localStorage.getItem("tables")).filter(
-        (x) => x.profileId === this.selectedProfile
-      );
-    },
-
     doneEdit() {
       this.isEdit = !this.isEdit;
-      this.tablesJoining();
     },
     removeTable(tableKey) {
       this.tables = this.tables.filter((x) => {
         return x.key !== tableKey;
       });
       this.lastDeleted = tableKey;
-      this.tablesJoining();
+      localStorage.setItem("ordis:tables", JSON.stringify(this.tables));
     },
     addProfile() {
       if (this.profileName === "") return false;
       this.profiles.push({
-        Id: Math.floor(Math.random() * 20) + 1,
+        id: Math.floor(Math.random() * 20) + 1,
         name: this.profileName,
       });
-      this.selectProfile(this.selectedProfile);
+      localStorage.setItem("ordis:planes", JSON.stringify(this.profiles));
+      this.selectedProfile = this.profiles.sort((a, b) => a.id - b.id)[0].id;
     },
     selectProfile(profileId) {
-      let localStorageItem = JSON.parse(localStorage.getItem("tables")).filter(
-        (x) => x !== null
-      );
       this.selectedProfile = profileId;
-      this.tables =
-        localStorageItem !== null && localStorageItem.length > 0
-          ? localStorageItem.filter((x) => x.profileId === this.selectedProfile)
-          : [];
     },
     hideModal() {
       this.$refs["profileModal"].hide();
@@ -255,19 +211,9 @@ export default {
     },
   },
   computed: {
-    validation() {
-      return (
-        this.profileName.length > 4 &&
-        this.profileName.length < 13 &&
-        this.profiles.some((x) => x.name === this.profileName) === false
-      );
-    },
-  },
-
-  created() {
-    this.tables = JSON.parse(localStorage.getItem("tables")).filter(
-      (x) => x.profileId === this.selectedProfile
-    );
+    profileTables() {
+      return this.tables.filter(table => table.profileId === this.selectedProfile) || [];
+    }
   },
 };
 </script>
